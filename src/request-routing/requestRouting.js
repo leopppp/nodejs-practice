8b681f659ad37accb61af1b2f634b8181682979d
Leo Peng <liangpeng@nbnco.com.au>
2020-03-30 18:39:27 +1100

    http request routing

---
+++ src/request-routing/requestRouting.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    console.log(`Request was made: ${req.url}`)
+    if(req.url === '/home' || req.url === '/'){
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        const readStrem = fs.createReadStream(`${__dirname}/index.html`,'utf8');
+        readStrem.pipe(res);
+    } else if(req.url === '/api/contacts'){
+        res.writeHead(200, {'Content-Type' : 'application/json'});
+        const ninjas = [{name: 'Tom', age: 28},{name: 'Jason', age:32}];
+        res.end(JSON.stringify(ninjas));
+    } else {
+        res.writeHead(404, {'Content-Type' : 'text/html'});
+        const readStrem = fs.createReadStream(`${__dirname}/404.html`,'utf8');
+        readStrem.pipe(res);
+    }
+}).listen(3000);
+
+console.log('Listening on port 3000...')
